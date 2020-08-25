4230572088a33e5f9ffa7434200f7d39438f6b51
Blake Powelson <blakepow@gmail.com>
2020-08-25 12:05:18 -0700

    update

--- mod1/node-calculator/main.js
+++ mod1/node-calculator/main.js
@@ -1,3 +1,35 @@
-var readlineSync = require('readline-sync');
-var name = readlineSync.question('What is your name? ')
-console.log(name)
+let readlineSync = require('readline-sync');
+let num1 = parseInt(readlineSync.question('Enter the first number: '));
+let num2 = parseInt(readlineSync.question('Enter the second number: '));
+function main(){
+    let operation = readlineSync.question('Enter the operation to perform (add, sub, mul, div): ');
+    function add(num1, num2){
+        console.log('The result is ' + (num1 + num2));
+    }
+    function sub(num1, num2){
+        console.log('The result is ' + (num1 - num2));
+    }
+    function mul(num1, num2){
+        console.log('The result is ' + (num1 * num2));
+    }
+    function div(num1, num2){
+        console.log('The result is ' + (num1 / num2));
+    }
+    if (operation === 'add'){
+        add(num1, num2);
+    }
+    else if (operation === 'sub'){
+        sub(num1, num2);
+    }
+    else if (operation === 'mul'){
+        mul(num1, num2);
+    }
+    else if (operation === 'div'){
+        div(num1, num2);
+    }
+    else{
+        main();
+    }
+}
+
+main();
