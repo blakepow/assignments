89d70b605ad68ddd7da96641b6622adc3de903f4
Blake Powelson <blakepow@gmail.com>
2020-11-02 16:55:28 -0700

    Half way through mod3

--- mod3/blog-props/src/components/App.js
+++ mod3/blog-props/src/components/App.js
@@ -1,26 +1,14 @@
 import React from 'react'
 
-import BlogList from './BlogList'
-import data from './info'
 import Header from './Header'
 import Footer from './Footer'
+import BlogPost from './BlogPost'
 
 export default function App() {
-  const entries = data.map(
-    entry => <BlogList
-      title = {entry.title}
-      subTitle = {entry.subTitle}
-      author = {entry.author}
-      date = {entry.date}
-    />
-  )
-  
-
-
   return (
     <div>
       <Header/>
-      {entries}
+      <BlogPost/>
       <button className='nextPage'>Older Post</button>
       <Footer/>
     </div>
